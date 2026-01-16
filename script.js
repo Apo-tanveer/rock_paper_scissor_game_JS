be1451a81f38565ce409c46424bc5910c6326a93
apo-tanveer <mynameisapo6@gmail.com>
2026-01-17 03:04:27 +0500

    added ai

--- script.js
+++ script.js
@@ -1,70 +1,51 @@
-function play_game(){
+    let para = document.querySelector("p")
+    let buttons = document.querySelectorAll("button");
 
- let computer_score = 0;
- let Your_score     = 0;
- let i= 0;
- while( i<5 ){
+    buttons.forEach(btn=>{
+        btn.addEventListener("click", (e)=>{
+//---------------USER---CHOICE-------------------///
+            let user_choice = 0;
+            
+            if(e.target.id === "rock"){
+                user_choice= 1;
+            }
+            else if(e.target.id === "paper"){
+                user_choice= 2;
+            }
+            else if(e.target.id === "scissor"){
+                user_choice= 3;
+            }
+        
+     
+    //--------------COMPUTER--CHOICE---------//
+            
+            function computerChoice(){
+   
+              return Math.floor (Math.random()*3) + 1;
+             }
 
-   function play_round(){
-       function computer_choice(){
-   
-       return Math.floor (Math.random()*3) + 1;
-       }
-   
-   
-       function user_choice(){
-       let a = prompt("            CHOOSE!\nRock       Paper       Scissor");
-           let b = a.toUpperCase();
-       
-           if (b!=="ROCK"&& b!=="PAPER"&& b!=="SCISSOR"){
-               alert("Plz enter correct word");
-               user_choice();
-           }
-           else if (b=="ROCK"){
-               return 1; 
-           }
-           else if (b==="PAPER"){
-               return 2; 
-           }
-           else{
-               return 3; 
-           }
-       }
-   
-       user_code = user_choice();
-       comp_code = computer_choice();
-   
-   
-       if (user_code===comp_code){
-           alert("Tie");
-       }
-       else if ((user_code=== 1 && comp_code===3) || (user_code===2 && comp_code===1) || (user_code===3 && comp_code===2)){
-           alert("You win");
-           Your_score+=1;
-           i+=1;
-       }
-       else if ((comp_code===1 && user_code===3) || (comp_code===2 && user_code===1) || ((comp_code===3 && user_code===2)) ){
-           alert("Robot won");
-           computer_score+=1;
-           i+=1;
-       }
-      
-   }
-   
-   play_round();
+            let computer_choice = computerChoice();
+    
 
- }
- if(Your_score>computer_score){
-    alert(`Congratulations! You won!!!\nYour's score: ${Your_score} \nRobot's score: ${computer_score}`);
- }
- else{
-    alert(`AWww! You lost :(\nYour's score: ${Your_score} \nRobot's score: ${computer_score}`);
- }
-if(confirm("Do you want to play again")){
-    play_game();
-}else{
-    alert("Good bye!");
-}
-}
+            if (user_choice===computer_choice){
+                  para.textContent= "Tie";
+               }
+            else if ((user_choice=== 1 && computer_choice===3) || (user_choice===2 && computer_choice===1) || (user_choice===3 && computer_choice===2)){
+                 para.textContent= "You won";
+                    
+             }
+            else if ((computer_choice===1 && user_choice===3) || (computer_choice===2 && user_choice===1) || ((computer_choice===3 && user_choice===2)) ){
+                 para.textContent= "Robot won";
+                
+             }
 
-play_game();
+
+
+
+
+        } )
+    })
+    
+
+
+    
---
+++ script1.js
@@ -0,0 +1,70 @@
+function play_game(){
+
+ let computer_score = 0;
+ let Your_score     = 0;
+ let i= 0;
+ while( i<5 ){
+
+   function play_round(){
+       function computer_choice(){
+   
+       return Math.floor (Math.random()*3) + 1;
+       }
+   
+   
+       function user_choice(){
+       let a = prompt("            CHOOSE!\nRock       Paper       Scissor");
+           let b = a.toUpperCase();
+       
+           if (b!=="ROCK"&& b!=="PAPER"&& b!=="SCISSOR"){
+               alert("Plz enter correct word");
+               user_choice();
+           }
+           else if (b=="ROCK"){
+               return 1; 
+           }
+           else if (b==="PAPER"){
+               return 2; 
+           }
+           else{
+               return 3; 
+           }
+       }
+   
+       user_code = user_choice();
+       comp_code = computer_choice();
+   
+   
+       if (user_code===comp_code){
+           alert("Tie");
+       }
+       else if ((user_code=== 1 && comp_code===3) || (user_code===2 && comp_code===1) || (user_code===3 && comp_code===2)){
+           alert("You win");
+           Your_score+=1;
+           i+=1;
+       }
+       else if ((comp_code===1 && user_code===3) || (comp_code===2 && user_code===1) || ((comp_code===3 && user_code===2)) ){
+           alert("Robot won");
+           computer_score+=1;
+           i+=1;
+       }
+      
+   }
+   
+   play_round();
+
+ }
+ if(Your_score>computer_score){
+    alert(`Congratulations! You won!!!\nYour's score: ${Your_score} \nRobot's score: ${computer_score}`);
+ }
+ else{
+    alert(`AWww! You lost :(\nYour's score: ${Your_score} \nRobot's score: ${computer_score}`);
+ }
+if(confirm("Do you want to play again")){
+    play_game();
+}else{
+    alert("Good bye!");
+}
+}
+
+play_game();
